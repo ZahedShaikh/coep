09a797deefc42e726238987a44ce1933c5c1ccda
Jahed Shaikh <jahed22shaikh@gmail.com>
2020-02-15 12:08:08 +0530

    COEP-1: URL clean-up

--- routes/web.php
+++ routes/web.php
@@ -33,32 +33,14 @@ Route::get('/profileprint', 'ProfilePrintController@show')->name('profileprint')
 Route::get('/getNewApplications', 'newApplicationsController@show')->name('getNewApplications');
 Route::post('/assignScholarships', 'assignScholarshipsController@update')->name('assignScholarships');
 
-
+// Send Sacntion amount to Account department for monry transfer
 Route::get('/getSanctionAmount', 'sanctionAmountController@show')->name('getSanctionAmount');
 Route::post('/sendSanctionAmount', 'sanctionAmountController@sendToAccounts')->name('sendSanctionAmount');
 
 
 
 
-
-
-//Done
-/*
-Route::get('/getScholarships', 'assignScholarshipsController@edit')->name('getScholarships');
-Route::post('/assignScholarships', 'assignScholarshipsController@update')->name('assignScholarships');
-Route::get('/amountDistro', 'transactionhistoryController@edit')->name('getamountDistro');
-Route::post('/assignamountDistro', 'transactionhistoryController@update')->name('assignamountDistro');
-
-// Wrokaround
-Route::get('/getStudentDataView', 'AllStudentDataViewController@ajaxRequest')->name('getStudentDataView');
-Route::get('/allStudentDataView', 'AllStudentDataViewController@index')->name('allStudentDataView');
-//Route::resource('allStudentDataView', 'AllStudentDataViewController');
-// Later to be done, if needed!
-Route::get('/displayAStudentDetail', 'displayAStudentDetailController@show')->name('displayAStudentDetail');
-//Route::get('/displayAll', 'displayAllController@show')->name('displayAll');
-//https://stackoverflow.com/questions/34217120/how-to-pass-multiple-arguments-with-url-routing-in-laravel-5-1
-*/
-
+// Temp URL to be remove later
 
 Route::resource('ajax-crud', 'AjaxController');
 //https://www.tutsmake.com/laravel-5-7-create-first-ajax-crud-application/
